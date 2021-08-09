c8962dde575e338f510356ee017155960661d010
Gabriela Shirley <gabrielashirleyh@gmail.com>
2021-08-08 21:42:08 -0700

    Signup Login setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { SignupComponent } from './auth/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: SignupComponent },
+  // { path: 'view-post/:id', component: ViewPostComponent },
+  // { path: 'user-profile/:name', component: UserProfileComponent, canActivate: [AuthGuard] },
+  // { path: 'list-subreddits', component: ListSubredditsComponent },
+  // { path: 'create-post', component: CreatePostComponent, canActivate: [AuthGuard] },
+  // { path: 'create-subreddit', component: CreateSubredditComponent, canActivate: [AuthGuard] },
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
